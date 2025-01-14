e0ebe887ab182156ee1fa9133e1bfd94e0065590
Juan Felipe Ortiz <juan@biosilico.io>
2025-01-14 11:08:56 -0500

    add oil

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -22,3 +22,5 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+--start oil with command
+vim.keymap.set('n', '<leader>F', '<CMD>Oil --float<CR>', { desc = 'See files with Oil' })
